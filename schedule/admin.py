37f5fe6dfe3e231d35a3d009769ccca61199f98f
Justin Quick <justquick@gmail.com>
2010-06-10 14:55:24 -0400

    set creator as a raw_id_field so it wont crash sites w/ tons of users (like ours)

--- schedule/admin.py
+++ schedule/admin.py
@@ -6,6 +6,9 @@ class CalendarAdminOptions(admin.ModelAdmin):
     prepopulated_fields = {"slug": ("name",)}
     search_fields = ['name']
 
+class EventAdmin(admin.ModelAdmin):
+    raw_id_fields = ("creator",)
 
 admin.site.register(Calendar, CalendarAdminOptions)
-admin.site.register([Rule, Event, CalendarRelation])
+admin.site.register(Event, EventAdmin)
+admin.site.register([Rule, CalendarRelation])
